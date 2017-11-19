85570fe12d5df93260ba288dece6db27c6b169e8
HitFsT <396433458@qq.com>
2017-11-20 01:13:08 +0800

    lab7

---
+++ src/application/BuildPtrTest.java
@@ -0,0 +1,49 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuildPtrTest {
+
+	@Test
+	public void testGetfile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBridgeWords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryBridgeWords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateNewText() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPathcollect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcShortestPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRandomWalk() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/application/MainTest.java
@@ -0,0 +1,64 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testLaunchClassOfQextendsApplicationStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHostServices() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParameters() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyPreloader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+}
